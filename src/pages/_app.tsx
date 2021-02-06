403fa85ea45e71e3251625bd94ee1ea198b89c0b
Takashi Sato <takashisato.235711@gmail.com>
2021-02-06 19:32:19 +0900

    chakraui導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@styles/theme';
 import '@styles/globals.css';
 
-export const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+export const App = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 );
 
-export default MyApp;
+export default App;
