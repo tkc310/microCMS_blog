5b671e9989e916f56233a4107517c0873fb2159b
Takashi Sato <takashisato.235711@gmail.com>
2021-02-20 20:46:14 +0900

    hightlight.js import方法を調整

--- src/lib/highlight.ts
+++ src/lib/highlight.ts
@@ -1,16 +1,17 @@
 import * as hljs from 'highlight.js';
+import css from 'highlight.js/lib/languages/css';
+import scss from 'highlight.js/lib/languages/scss';
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+import json from 'highlight.js/lib/languages/json';
+import ruby from 'highlight.js/lib/languages/ruby';
+import python from 'highlight.js/lib/languages/python';
+import go from 'highlight.js/lib/languages/go';
 
-[
-  'css',
-  'scss',
-  'javascript',
-  'typescript',
-  'json',
-  'ruby',
-  'python',
-  'go',
-].forEach((lang: string) => {
-  hljs.registerLanguage(lang, require(`highlight.js/lib/languages/${lang}`));
-});
+[css, scss, javascript, typescript, json, ruby, python, go].forEach(
+  (lang: string) => {
+    hljs.registerLanguage(`${lang}`, lang);
+  }
+);
 
 export default hljs;
