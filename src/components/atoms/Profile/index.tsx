de2a65a4baf555776e9cfd75f5ed15c8ddb67df4
Takashi Sato <takashisato.235711@gmail.com>
2021-02-21 18:16:18 +0900

    lighthouseの指摘修正 画像サイズ指定

--- src/components/atoms/Profile/index.tsx
+++ src/components/atoms/Profile/index.tsx
@@ -18,7 +18,7 @@ export const Profile = ({ profile }: Props) => {
     <div>
       <div className="l-footer_profile">
         <div className="l-footer_icon">
-          <img src={image} width="90" alt="プロフィール画像" />
+          <img src={image} width="90" height="90" alt="プロフィール画像" />
         </div>
         <div className="l-footer_text">
           <div className="l-footer_name">{name}</div>
